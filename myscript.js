08b87a7b42e0dd5ec8f7e5f1bf3a944bfc042d74
Promise_digitals <promiseedeh025@gmail.com>
2023-11-13 12:16:38 -0800

    added images and updated style.css

--- myscript.js
+++ myscript.js
@@ -4,7 +4,7 @@ var logo = document.getElementById('logo');
 icon.onclick = function(){
     document.body.classList.toggle('dark-mode');
     if (document.body.classList.contains('dark-mode')) {
-        icon.src = 'images/sun.png';
+        icon.src = 'images/sun.ico';
         logo.src = 'images/logo 1.png';
     } else {
         icon.src = 'images/moon.png';
