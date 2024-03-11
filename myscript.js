6c37e8c87dd2af8ebdfb841030b1a266e4775eee
Promise-Digitals <promiseedeh025@gmail.com>
2024-03-11 03:32:57 -0700

    updated the folder

--- myscript.js
+++ myscript.js
@@ -5,9 +5,9 @@ icon.onclick = function(){
     document.body.classList.toggle('dark-mode');
     if (document.body.classList.contains('dark-mode')) {
         icon.src = 'images/sun.ico';
-        logo.src = 'images/logo 1.png';
+        logo.src = 'images/logo-2.png';
     } else {
         icon.src = 'images/moon.png';
-        logo.src = 'images/logo 2.png';
+        logo.src = 'images/logo-1.png';
     }
 }
